e6384a5df2cac2bc610084944c0db3775d8ef233
ramadhaniwanjenja <r.wanjenja@alustudent.com>
2025-03-27 01:49:07 +0200

    Add logging to verify-reset-token route

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -7,7 +7,11 @@ router.post('/signup', authController.signup);
 router.post('/login', authController.login);
 router.get('/verify-email', authController.verifyEmail);
 router.post('/request-password-reset', authController.requestPasswordReset);
-router.get('/verify-reset-token', authController.verifyResetToken);
+router.get('/verify-reset-token', (req, res, next) => {
+  console.log('Reached /api/auth/verify-reset-token route'); // Debug
+  console.log('Query token:', req.query.token); // Debug
+  authController.verifyResetToken(req, res, next);
+});
 router.post('/reset-password', authController.resetPassword);
 router.post('/resend-verification', authController.resendVerification);
 router.get('/users', [verifyToken, isAdmin], authController.getAllUsers);
