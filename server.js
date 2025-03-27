a2d4d5581ff74a6d56280380bc0f91acc9146679
ramadhaniwanjenja <r.wanjenja@alustudent.com>
2025-03-27 16:13:31 +0200

    change deploy site to netlify

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ dotenv.config();
 app.use(cors({
   origin: (origin, callback) => {
     const allowedOrigins = [
-      'https://levkonnects.vercel.app',
+      'https://levkonnect.netlify.app/',
     ];
     if (!origin || allowedOrigins.includes(origin)) {
       callback(null, true);
